9523edd85ba1a44b42ba05b38065b93e1e3201b6
Jeremy Blanchard <blanchard.jeremy@gmail.com>
2013-06-10 12:47:18 -0700

    Override the account creation process and language

--- exampleapp/views.py
+++ exampleapp/views.py
@@ -1,9 +1,17 @@
 from django.core.urlresolvers import reverse
 from sky_visitor.views import InvitationStartView, InvitationCompleteView
+from django.utils.translation import ugettext_lazy as _
 
 
 class CustomInvitationStartView(InvitationStartView):
     template_name = 'exampleapp/invitation_start.html'
+    success_message = _("Invitation successfully delivered. Invite more friends!")
+
+    def get_email_kwargs(self, user):
+        kwargs = super(CustomInvitationStartView, self).get_email_kwargs(user)
+        # Custom subject line
+        kwargs['subject'] = "Your friend invited you to join the community!"
+        return kwargs
 
 
 class CustomInvitationCompleteView(InvitationCompleteView):
